9f9eb6cfab3e99188da434cf16340cd350b9297a
Josh Cousins <josh.cousins@climetric.design>
2025-02-04 18:01:24 +0200

    6

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+//Test
+
 export default function Register() {
   const [fullName, setFullName] = useState("");
   const [password, setPassword] = useState("");
